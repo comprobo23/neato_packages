be7365bc17905016000c5bef869da8364b07fddf
Paul Ruvolo <paullundyruvolo@gmail.com>
2022-08-29 17:29:43 -0400

    forgot to import something

--- neato_node2/launch/bringup.py
+++ neato_node2/launch/bringup.py
@@ -1,3 +1,4 @@
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
