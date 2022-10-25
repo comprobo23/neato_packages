ae35ba3981910e7313fc660c50f4f11c549f24a1
Paul Ruvolo <paullundyruvolo@gmail.com>
2022-10-25 17:18:54 -0400

    added the ability to change port (a bit clunky currently)

--- neato_node2/launch/bringup_multi.py
+++ neato_node2/launch/bringup_multi.py
@@ -1,3 +1,9 @@
+"""
+Sample invocation:
+
+ros2 launch neato_node2 bringup_multi.py host:=192.168.16.59 robot_name:=a udp_video_port:=5002 gscam_config:='udpsrc port=5002 ! application/x-rtp, payload=96 ! rtpjitterbuffer ! rtph264depay ! avdec_h264  ! videoconvert'
+"""
+
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
@@ -10,6 +16,8 @@ import os
 def generate_launch_description():
     use_udp = DeclareLaunchArgument('use_udp', default_value="true")
     host = DeclareLaunchArgument('host', default_value="")
+    gscam_config = DeclareLaunchArgument('gscam_config', default_value='udpsrc port=5002 ! application/x-rtp, payload=96 ! rtpjitterbuffer ! rtph264depay ! avdec_h264  ! videoconvert')
+    udp_video_port = DeclareLaunchArgument('udp_video_port', default_value="5002")
     robot_name = LaunchConfiguration('robot_name')
     robot_name_command = DeclareLaunchArgument('robot_name', default_value='')
     interfaces_launch_file_dir = os.path.join(get_package_share_directory('neato2_gazebo'), 'launch')
@@ -17,6 +25,8 @@ def generate_launch_description():
     return LaunchDescription([
         use_udp,
         host,
+        gscam_config,
+        udp_video_port,
         robot_name_command,
         GroupAction(actions=[PushRosNamespace(namespace=robot_name),
            Node(
@@ -42,7 +52,7 @@ def generate_launch_description():
                 package='neato_node2',
                 executable='setup_udp_stream',
                 name='udp_stream_setup',
-                parameters=[{"receive_port": 5000},
+                parameters=[{"receive_port": LaunchConfiguration('udp_video_port')},
                             {"width": 1024},
                             {"height": 768},
                             {"fps": 30},
@@ -57,7 +67,7 @@ def generate_launch_description():
                     {'camera_name': 'camera'},
                     {'use_gst_timestamps': False},
                     {'frame_id': 'camera'},
-                    {'gscam_config': 'udpsrc port=5000 ! application/x-rtp, payload=96 ! rtpjitterbuffer ! rtph264depay ! avdec_h264  ! videoconvert'}
+                    {'gscam_config': LaunchConfiguration('gscam_config')}
                 ]
             )
         ])
